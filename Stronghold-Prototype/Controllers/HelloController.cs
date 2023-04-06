bdb142abefdc781f93a10400cc129a70c00a098e
wwlowie <125466068+wwlowie@users.noreply.github.com>
2023-04-06 17:34:22 +0700

    Add project files.

---
+++ Stronghold-Prototype/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Stronghold_Prototype.Controllers
+{
+    [RoutePrefix("api/hello")]
+    public class HelloController : ApiController
+    {
+        [Route]
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return BadRequest("Hello World!"); // "Hello World!";
+
+        }
+    }
+}
---
+++ Stronghold-Prototype/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Stronghold_Prototype.WebApiApplication" Language="C#" %>
