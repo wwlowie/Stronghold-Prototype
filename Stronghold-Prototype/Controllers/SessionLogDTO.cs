1ca5cbce8202689741a697528f77e6e5e1e10167
wwlowie <125466068+wwlowie@users.noreply.github.com>
2023-04-18 12:34:15 +0700

    DCrealizerepos

---
+++ Stronghold-Prototype/Controllers/SessionLogDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Stronghold_Prototype.Controllers
+{
+    public class SessionLogDTO
+    {
+        public string topicID;
+        public string sessionDateISO;
+        public string sessionTimeISO;
+        public string durationISO;
+        public string recorderName;
+        public List<string> mentorDiscordUserIds;
+        public List<attendeesGTO> attendees;
+    }
+
+    public class attendeesGTO
+    {
+        public string discordUserId;
+        public string attendanceDurationISO;
+    }
+}
---
+++ Stronghold-Prototype/Controllers/SessionLogsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Stronghold_Prototype.Controllers
+{
+    [RoutePrefix("api/session-logs")]
+    public class SessionLogsController: ApiController
+    {
+        [Route]
+        [HttpPost]
+        public IHttpActionResult CreateSessionLog([FromBody] SessionLogDTO sessionLogDTO)
+        {
+            if (sessionLogDTO == null)
+                return BadRequest();
+            return Ok(sessionLogDTO);
+        }
+    }
+
+    
+}
